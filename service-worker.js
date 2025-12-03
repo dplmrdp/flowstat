11c59ec76b6db366c99290ccb73b2e658116414e
dplmrdp <dplmrdp@gmail.com>
2025-12-03 15:01:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,29 @@ const urlsToCache = [
   '/flowstat/manifest.json'
 ];
 
+// SERVICE WORKER - MODO DESARROLLO
+// =================================
+// Este SW NO utiliza caché.
+// Siempre carga la última versión de la app desde el servidor.
+// Ideal para desarrollo. NO apto para producción.
+
+self.addEventListener('install', (event) => {
+  // Activar automáticamente sin esperar
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  // Tomar control inmediato sobre todas las pestañas
+  event.waitUntil(clients.claim());
+});
+
+// Interceptar todas las peticiones y NO hacer caching.
+// Siempre hace fetch directo.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
+
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
