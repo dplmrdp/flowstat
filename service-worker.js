38255c7a342626676c4f37cc6012bc6c90bb382c
dplmrdp <dplmrdp@gmail.com>
2025-12-03 10:55:23 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'flowstat-cache-v1';
+const urlsToCache = [
+  '/flowstat/',
+  '/flowstat/index.html',
+  '/flowstat/style.css',
+  '/flowstat/app.js',
+  '/flowstat/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
